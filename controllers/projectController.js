9ae71814bce24eb5a89a34a5c085ff274ddf4f3f
Rodrigo Alves Guerra <rodrigoa.guerra@hotmail.com>
2019-08-07 11:35:28 -0300

    Use JWT on Auth

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import authMiddleware from "../middlewares/auth";
 
 const router = express.Router();
 
+router.use(authMiddleware);
+
 router.get('/', (req, res) => {
-  res.send({ ok: true });
+  res.send({ ok: true , user: req.userId });
 });
 
 module.exports = app => app.use('/projects', router);
